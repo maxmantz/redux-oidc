3a0c7845d13a39a4a201ab33805d721ab1800768
Maximilian Mantz <maximilian.mantz@yahoo.de>
2016-04-09 10:13:09 +0200

    logout methods added

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,15 @@
 import OidcTokenManager from '../libs/oidc-token-manager';
 
-export default function createTokenManager(config) {
+export function createTokenManager(config) {
   return new OidcTokenManager(config);
-};
+}
+
+export function logoutAtIdentityService(config) {
+  const manager = new OidcTokenManager(config);
+  manager.redirectForLogout();
+}
+
+export function logout() {
+  const manager = new OidcTokenManager();
+  manager.removeToken();
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import OidcTokenManager from '../libs/oidc-token-manager';
 import createTokenMiddleware from './tokenMiddleware';
 import CallbackComponent from './CallbackComponent';
-import createTokenManager from './helpers';
+import {createTokenManager, logout, logoutAtIdentityService} from './helpers';
 
-export { CallbackComponent, createTokenManager };
+export { CallbackComponent, createTokenManager, logout, logoutAtIdentityService };
 
 export default createTokenMiddleware;
