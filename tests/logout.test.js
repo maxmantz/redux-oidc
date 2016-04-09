15456ec9daf1566b8d43ebb46e2f8291656fdc6e
Maximilian Mantz <maximilian.mantz@yahoo.de>
2016-04-09 10:56:51 +0200

    Added tests for the logout helpers

---
+++ tests/logout.test.js
@@ -0,0 +1,25 @@
+import sinon from 'sinon';
+import { logout, createTokenManager } from '../src/helpers';
+import expect from 'expect';
+
+describe('logout()', () => {
+  let removeTokenStub;
+  let createTokenManagerStub;
+  beforeEach(() => {
+    const tokenManager = createTokenManager();
+    removeTokenStub = sinon.stub(tokenManager, 'removeToken');
+    removeTokenStub.returns(null);
+    createTokenManagerStub = sinon.stub(createTokenManager);
+    createTokenManagerStub.returns(tokenManager);
+  });
+
+  afterEach(() => {
+    removeTokenStub.restore();
+    createTokenManagerStub.restore();
+  });
+
+  it('should call the removeToken function', () => {
+    logout();
+    expect(removeTokenStub.called).toBe(true)
+  });
+});
---
+++ tests/logoutAtIdentityService.test.js
@@ -0,0 +1,19 @@
+import { logoutAtIdentityService, createTokenManager } from '../src/helpers';
+import expect from 'expect';
+
+describe('logoutAtIdentityService(config)', () => {
+  let redirectForLogoutStub;
+  let createTokenManagerStub;
+  beforeEach(() => {
+    const tokenManager = createTokenManager();
+    redirectForLogoutStub = sinon.stub(tokenManager, 'redirectForLogout');
+    removeTokenStub.returns(null);
+    createTokenManagerStub = sinon.stub(createTokenManager);
+    createTokenManagerStub.returns(tokenManager);
+  });
+
+  it('should call the redirectForLogout method', () => {
+    logoutAtIdentityService();
+    expect(redirectForLogoutStub.called).toBe(true);
+  });
+});
