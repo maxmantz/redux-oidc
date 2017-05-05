75362a40bebff221a9031d38a0d264d8974d7c3e
Brandon Boone <BranAndJus@gmail.com>
2017-05-05 15:42:00 -0400

    Fix for PropTypes warning in React 15.5
    Fixes: "Warning: Accessing PropTypes via the main React package is
    deprecated. Use the prop-types package from npm instead."

--- src/OidcProvider.js
+++ src/OidcProvider.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React from 'react';
 import { userExpired, userFound, silentRenewError, sessionTerminated, userExpiring, userSignedOut } from './actions';
 
 class OidcProvider extends React.Component {
