26d2505bc108befaa76dae9e66983be11c541c88
Maximilian Mantz <maximilian.mantz@yahoo.de>
2017-05-08 20:51:50 +0200

    post-publish v3.0.0-beta.7

--- src/helpers/loadUser.js
+++ src/helpers/loadUser.js
@@ -2,6 +2,14 @@ import co from 'co';
 import { userFound, userExpired } from '../actions';
 
 export function* loadUserHandler(store, userManager) {
+  if (!store || !store.dispatch) {
+    throw new Error('redux-oidc: You need to pass the redux store into the loadUser helper!');
+  }
+
+  if (!userManager || !userManager.getUser) {
+    throw new Error('redux-oidc: You need to pass the userManager into the loadUser helper!');
+  }
+
   const user = yield userManager.getUser();
 
   if (user && !user.expired) {
