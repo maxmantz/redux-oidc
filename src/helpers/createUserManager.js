06241fb2a43905c2fbc83b54de657d09a03e5b46
Maximilian Mantz <maximilian.mantz@yahoo.de>
2017-09-28 12:43:48 +0200

    post-publish v3.0.0-beta.11

--- src/helpers/createUserManager.js
+++ src/helpers/createUserManager.js
@@ -1,4 +1,15 @@
-import { UserManager } from 'oidc-client';
+// check that the UserManager is present
+let UserManager;
+
+try {
+  UserManager = require('oidc-client').UserManager;
+} catch (e) {
+  try {
+    UserManager = require('oidc-client-fetch').UserManager;
+  } catch (e) {
+    throw new Error("redux-oidc: Couldn't find UserManager. Please install either 'oidc-client' or 'oidc-client-fetch'.");
+  }
+}
 
 export default function createUserManager(config) {
   return new UserManager(config);
