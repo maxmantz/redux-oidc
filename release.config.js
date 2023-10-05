6bf950fe9e48b51c7c4cdf73a228708213fa0706
Yogesh Surendran <carreraphp@gmail.com>
2023-10-06 03:36:41 +0530

    build: add semantic release changelog package

--- release.config.js
+++ release.config.js
@@ -3,7 +3,12 @@ module.exports = {
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
+      [
+        "@semantic-release/changelog",
+        {
+          "changelogFile": "docs/CHANGELOG.md"
+        }
+      ],
       '@semantic-release/npm',
       '@semantic-release/github',
     ],
