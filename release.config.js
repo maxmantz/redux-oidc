63aa352351c89283756c2a0b2d25f752fa495fc4
Yogesh Surendran <carreraphp@gmail.com>
2023-10-06 04:30:47 +0530

    builb: update npm publish access to public

--- release.config.js
+++ release.config.js
@@ -1,16 +1,25 @@
 module.exports = {
-    branches: ['master'],
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      [
-        "@semantic-release/changelog",
-        {
-          "changelogFile": "docs/CHANGELOG.md"
-        }
-      ],
-      '@semantic-release/npm',
-      '@semantic-release/github',
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
     ],
-  };
-  
+    [
+      "@semantic-release/npm",
+      {
+        npmPublish: true,
+        tarballDir: "dist",
+        // Add the following line to publish as a public package
+        publishConfig: {
+          access: "public",
+        },
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
